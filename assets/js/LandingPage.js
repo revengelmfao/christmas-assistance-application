61ba3e69ed9991b941f170fcc760f6095cd630ca
Peter Rudolf <peterrudolf249@gmail.com>
2024-11-13 20:06:25 -0500

    fix/debug of javascript

--- assets/js/LandingPage.js
+++ assets/js/LandingPage.js
@@ -1,53 +1,39 @@
 // variable declaration
-const sponsorModal = document.getElementById('sponsorModal');
-const familyModal = document.getElementById('childSignupModal');
-const sponsorBtn = sponsorModal.querySelector('btn btn-primary');
-const familyBtn = familyModal.querySelector('btn btn-primary');
-const sponsorName = document.getElementById('sponsorName');
-const numChildren = document.getElementById('numChildren');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const childName = document.getElementById('childName');
-const childAge = document.getElementById('childAge');
-const childGender = document.getElementById('childGender');
-const childInterest = document.getElementById('childInterest');
+const sponsorForm = document.getElementById('sponsorForm');
+const childForm = document.getElementById('childSignupForm');
 
 
 // Sponsor event listener
-sponsorBtn.addEventListener('click', function() {
+sponsorForm.addEventListener('submit', function() {
 
-    // Check if all fields are filled out
-    if (!sponsorName.value || !numChildren.value || !email.value || !phone.value) {
-        alert('Please fill out all fields');
-        return;
-    }
+    // Create object with sponsor data and store in local storage
     sponsorFormHandler();
 });
 
-// Family event listener
-familyBtn.addEventListener('click', function() {
+// Child event listener
+childForm.addEventListener('submit', function() {
     
-    // Check if all fields are filled out, and the list of preferred gifts does not go over five
+    // Check that the list of preferred gifts does not go over five
+    const childInterest = document.getElementById('preferredGifts');
     let interestList = childInterest.value.split(',');
-    if (!childName.value || !childGender.value || !childAge.value || !childInterest.value) {
-        alert('Please fill out all fields');
-        return;
-    } else if (interestList.length > 5) {
+    
+    if (interestList.length > 5) {
         alert('Please list no more than five preferred gifts');
         return;
     }
 
     // Create object with child data
     const newChildData = {
-        childName: childName.value.trim(),
-        childGender: childGender.value,
-        childAge: childAge.value,
-        childInterest: interestList
+        childName: document.getElementById('childName').value.trim(),
+        childGender: document.getElementById('gender').value,
+        childAge: document.getElementById('age').value,
+        childInterest: document.getElementById('preferredGifts').value,
     }
     
     // get existing family data and add new entry
     const childData = readLocalStorage('families');
     childData.push(newChildData);
+    console.log(childData);
     saveToLocalStorage('families', childData);
 
     // Redirect to thank you page for child
